9f46a77365ef11d9b3a2cc3242cce3b2626b0eae
GreatHellizar23th <greenavalonbarrel23@gmail.com>
2021-03-07 19:55:05 +0300

    ADDED GUI for LoginViewController

--- Instagramm/Resources/Extensions.swift
+++ Instagramm/Resources/Extensions.swift
@@ -13,6 +13,26 @@ extension UIView {
         return frame.size.width
     }
 
+    public var height: CGFloat {
+        return frame.size.height
+    }
+
+    public var top: CGFloat {
+        return frame.origin.y
+    }
+
+    public var bottom: CGFloat {
+        return frame.origin.y + frame.size.height
+    }
+
+    public var left: CGFloat {
+        return frame.origin.x
+    }
+
+    public var right: CGFloat {
+        return frame.origin.x + frame.size.width
+    }
+
     //MARK: - Methods
     public func addSubviews(views: [UIView]) {
         for view in views {
