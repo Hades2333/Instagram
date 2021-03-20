5d698a8c1494cd6a91d34a4123398f8a3f95f7c2
Nikolai Beklemishchev <greenavalonbarrel23@gmail.com>
2021-03-20 20:53:22 +0300

    configured custom cells for HomeFeed & Post

--- Instagramm/Controllers/Core Tabs/HomeViewController.swift
+++ Instagramm/Controllers/Core Tabs/HomeViewController.swift
@@ -39,7 +39,7 @@ class HomeViewController: UIViewController {
     }
 
     private func createMockModels() {
-        let user = User(username: "Joe", bio: "",
+        let user = User(username: "@Joe", bio: "",
                         name: (first: "", last: ""),
                         profilePhoto: URL(string: "https://www.google.com")!,
                         birthdate: Date(),
@@ -157,6 +157,8 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             switch headerModel.renderType {
             case .header(let user):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostHeaderTableViewCell.identifier, for: indexPath) as! IGFeedPostHeaderTableViewCell
+                cell.delegate = self
+                cell.configure(with: user)
                 return cell
             case .comments, .actions, .primaryContent:
             return UITableViewCell()
@@ -168,6 +170,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             switch postModel.renderType {
             case .primaryContent(let post):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostTableViewCell.identifier, for: indexPath) as! IGFeedPostTableViewCell
+                cell.configure(with: post)
                 return cell
             case .header, .actions, .comments:
                 return UITableViewCell()
@@ -179,6 +182,7 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
             switch actionModel.renderType {
             case .actions(let provider):
                 let cell = tableView.dequeueReusableCell(withIdentifier: IGFeedPostActionsTableViewCell.identifier, for: indexPath) as! IGFeedPostActionsTableViewCell
+                cell.delegate = self
                 return cell
             case .comments, .header, .primaryContent:
                 return UITableViewCell()
@@ -224,4 +228,41 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
         let subSection = section % 4
         return subSection == 3 ? 70 : 0
     }
+}
+
+extension HomeViewController: IGFeedPostHeaderTableViewCellDelegate {
+    func didTapMoreButton() {
+        let actionSheet = UIAlertController(title: "Post options",
+                                            message: nil,
+                                            preferredStyle: .actionSheet)
+        actionSheet.addAction(UIAlertAction(title: "Report post",
+                                            style: .destructive,
+                                            handler: { [weak self] _ in
+                                                self?.reportPost()
+                                            }))
+        actionSheet.addAction(UIAlertAction(title: "Cancel",
+                                            style: .cancel,
+                                            handler: nil))
+        present(actionSheet, animated: true)
+    }
+
+    func reportPost() {
+
+    }
+}
+
+extension HomeViewController: IGFeedPostActionsTableViewCellDelegate {
+    func didTapLikeButton() {
+        print("like")
+    }
+
+    func didTapCommentButton() {
+        print("comment")
+    }
+
+    func didTapSendButton() {
+        print("send")
+    }
+
+
 }
--- Instagramm/Views/Cells/Home Feed & Post/IGFeedPostTableViewCell.swift
+++ Instagramm/Views/Cells/Home Feed & Post/IGFeedPostTableViewCell.swift
@@ -5,27 +5,64 @@
 //  Created by Hellizar on 14.03.21.
 //
 
+import AVFoundation
 import UIKit
+import SDWebImage
 
+/// Cell for primarary post content
 final class IGFeedPostTableViewCell: UITableViewCell {
 
     static let identifier = "IGFeedPostTableViewCell"
 
+    private var player: AVPlayer?
+    private var playerLayer = AVPlayerLayer()
+
+    private let postImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = nil
+        imageView.clipsToBounds = true
+        return imageView
+    }()
+
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        contentView.backgroundColor = .secondarySystemBackground
+        contentView.layer.addSublayer(playerLayer)
+        contentView.addSubview(postImageView)
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
 
-    public func configure() {
+    public func configure(with post: UserPost) {
+        postImageView.image = UIImage(named: "test")
         // configure the cell
+
+        return
+        
+        switch post.postType {
+        case .photo:
+            postImageView.sd_setImage(with: post.postURL, completed: nil)
+        // show image
+        case .video:
+        // load and play video
+            player = AVPlayer(url: post.postURL)
+            playerLayer.player = player
+            playerLayer.player?.volume = 0
+            playerLayer.player?.play()
+        }
     }
 
     override func layoutSubviews() {
         super.layoutSubviews()
+        playerLayer.frame = contentView.bounds
+        postImageView.frame = contentView.bounds
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        postImageView.image = nil
     }
     
 }
